3935641227a425df2b3866922c247c9a90bc6700
Christian Johnson-Richards <cjohnsonrichards@gmail.com>
2018-07-04 09:44:02 +0100

    Switched pins around
    I thought it best to leave pins D2 and D3 unuses for digital interupts as I may use this method for something in the future

--- 3_Shot_Burst/3_Shot_Burst.ino
+++ 3_Shot_Burst/3_Shot_Burst.ino
@@ -4,16 +4,16 @@ int counter;   //Keeps track of shots
 
 void setup() {
   //configure pin 2 as an input and enable the internal pull-up resistor
-  pinMode(2, INPUT_PULLUP); //Trigger switch input.
-  pinMode(3, OUTPUT);       //Mosfet trigger output.
+  pinMode(4, INPUT_PULLUP); //Trigger switch input.
+  pinMode(5, OUTPUT);       //Mosfet trigger output.
   //Serial.begin(9600);       //Debugging
 }
 
 void loop() {
-  digitalWrite(3, LOW); //Start with the motor off
+  digitalWrite(5, LOW); //Start with the motor off
   counter = 0; //Start with counter at zero.
-  while (digitalRead(2) == LOW) {   //While button is pressed turn on the motor.
-    digitalWrite(3, HIGH);
+  while (digitalRead(4) == LOW) {   //While button is pressed turn on the motor.
+    digitalWrite(5, HIGH);
     
     int  lightgate = analogRead(A0); //Read the light sensor.
     float lightgateV = lightgate * (5.0 / 1023.0); //Convert to volts.
@@ -24,12 +24,12 @@ void loop() {
     if (lightgateV >= 2 && lightgateV < 2.3) { 
       //Serial.println(lightgateV);
       counter = counter + 1; //Inc counter by 1.
-      delay(50); //This just adds a small delay in order for the gears
+      delay(70); //This just adds a small delay in order for the gears
       //to cach up as the arduino is too fast! 
      }
     if (counter >= burst) {
-      digitalWrite(3, LOW); //Motor off
-      while(digitalRead(2) == LOW) { //Loop here until trigger is released.
+      digitalWrite(5, LOW); //Motor off
+      while(digitalRead(4) == LOW) { //Loop here until trigger is released.
         delay(1);
         }  
     }
--- Single_Shot/Single_Shot.ino
+++ Single_Shot/Single_Shot.ino
@@ -1,25 +1,25 @@
 void setup() {
   //configure pin 2 as an input and enable the internal pull-up resistor.
-  pinMode(2, INPUT_PULLUP); //Trigger switch input.
-  pinMode(3, OUTPUT);       //Mosfet trigger output.
-  //Serial.begin(9600);       //Debugging.
+  pinMode(4, INPUT_PULLUP); //Trigger switch input.
+  pinMode(5, OUTPUT);       //Mosfet trigger output.
+  Serial.begin(9600);       //Debugging.
 }
 
 void loop() {
-  digitalWrite(3, LOW); //Start with the motor off
+  digitalWrite(5, LOW); //Start with the motor off
 
-  while (digitalRead(2) == LOW) {   //While button is pressed turn on the motor.
-    digitalWrite(3, HIGH);
+  while (digitalRead(4) == LOW) {   //While button is pressed turn on the motor.
+    digitalWrite(5, HIGH);
     
     int  lightgate = analogRead(A0); //Monitor the light sensor.
     float lightgateV = lightgate * (5.0 / 1023.0); //Convert to volts.
 
     //Catch the sector gear after firing a bb.
-    if (lightgateV > 3.3 && lightgateV < 3.6) {   
-        digitalWrite(3, LOW);       //Motor off imidiatly.
-        //Serial.println("Shot");   //Debugging
-        //Serial.println(lightgateV);
-        while(digitalRead(2) == LOW) {  //Loop here until trigger is released.
+    if (lightgateV > 3.3 && lightgateV < 4.5) {   
+        digitalWrite(5, LOW);       //Motor off imidiatly.
+        Serial.println("Shot");   //Debugging
+        Serial.println(lightgateV);
+        while(digitalRead(4) == LOW) {  //Loop here until trigger is released.
           delay(1);
         }
      }
